8aa0ba7f9438014dc5eb647f4502e77a426dbc8f
numumotik <numumotik@mail.ru>
2017-12-14 22:39:46 +0300

    add menu buttons

--- core/src/com/mygdx/panzer/MainMenuScreen.java
+++ core/src/com/mygdx/panzer/MainMenuScreen.java
@@ -13,7 +13,10 @@ import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.SelectBox;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextField;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 
 /**
@@ -21,15 +24,26 @@ import com.badlogic.gdx.utils.viewport.FitViewport;
  */
 
 public class MainMenuScreen extends ScreenAdapter {
-    final int WIDTH = 600;
-    final int HEIGHT = 600;
+    final int WIDTH = 1920;
+    final int HEIGHT = 1080;
     private PanzerProject process;
     private OrthographicCamera camera;
     private SpriteBatch batch;
     private FitViewport viewport;
     private Stage stage;
+    private Skin skin;
+    private TextField velocityField;
+    private TextField sensorField;
+    private TextField angleField;
+    private SelectBox<String> mapField;
+    private Texture enableSensorsTexture;
+    private Texture disableSensorsTexture;
+    private Image enableSensors;
     private Image applyButton;
     private Texture applyButtonTexture;
+    private Texture labelTexture;
+    private Image label;
+    private  BitmapFont font;
 
     public MainMenuScreen(final PanzerProject panz)
     {
@@ -41,6 +55,14 @@ public class MainMenuScreen extends ScreenAdapter {
         viewport.apply(true);
         batch = new SpriteBatch();
         this.stage = new Stage(viewport, batch);
+        skin = new Skin(Gdx.files.internal("uiskin.json"));
+        skin.getFont("default-font").getData().setScale(2);
+
+        enableSensorsTexture = new Texture("enabledSensors.png");
+        disableSensorsTexture = new Texture("disabledSensors.png");
+        Label drawsensors = new Label("Draw sensors", skin);
+        drawsensors.setFontScale(2);
+        enableSensors = new Image(enableSensorsTexture);
 
         applyButtonTexture = new Texture("ApplyButton.png");
         applyButton = new Image(applyButtonTexture);
@@ -52,10 +74,62 @@ public class MainMenuScreen extends ScreenAdapter {
             }
         });
 
+
+        Label velocity = new Label("Max Velocity:", skin); velocity.setFontScale(2);
+        Label sensor = new Label("Max sensors range:", skin); sensor.setFontScale(2);
+        Label angle = new Label("Start angle:", skin); angle.setFontScale(2);
+        Label mmap = new Label("Select map:", skin); mmap.setFontScale(2);
+        velocityField = new TextField("50", skin);
+        sensorField = new TextField("500", skin);
+        angleField = new TextField("0", skin);
+
+        //TODO: вынести в класс карт
+        String[] maps = new String[] {"grassmap", "lavamap"};
+        mapField = new SelectBox(skin);
+        mapField.setItems(maps);
+        labelTexture = new Texture("label.png");
+        label = new Image(labelTexture);
+
+        float space = 180;
         Table table = new Table();
         table.setPosition(0, 0);
         table.setSize(WIDTH, HEIGHT);
-        table.add(applyButton);
+
+        float wspace = 70;
+        float wsize = (WIDTH - wspace *5) / 4f;
+        float hsize = HEIGHT - 2*space - labelTexture.getHeight() - applyButton.getHeight() - 20;
+        Table inner1 = new Table();
+        inner1.setSize(WIDTH, hsize);
+        inner1.add().padRight(wspace);
+        inner1.add(velocity).size(wsize, hsize*0.3f); inner1.add().padRight(wspace);
+        inner1.add(sensor).size(wsize, hsize*0.3f); inner1.add().padRight(wspace);
+        inner1.add(angle).size(wsize, hsize*0.3f); inner1.add().padRight(wspace);
+        inner1.add(mmap).size(wsize, hsize*0.3f); inner1.add().padRight(wspace);
+        inner1.row();
+        inner1.add().padRight(wspace);
+        inner1.add(velocityField).size(wsize, hsize*0.7f); inner1.add().padRight(wspace);
+        inner1.add(sensorField).size(wsize, hsize*0.7f); inner1.add().padRight(wspace);
+        inner1.add(angleField).size(wsize, hsize*0.7f); inner1.add().padRight(wspace);
+        inner1.add(mapField).size(wsize, hsize*0.7f); inner1.add().padRight(wspace);
+
+        Table inner2 = new Table();
+        inner2.setSize(WIDTH, applyButtonTexture.getHeight() + 20);
+        inner2.row(); inner2.add().padRight(10);
+        inner2.add(enableSensors); inner2.add(drawsensors);
+        inner2.add().padRight(WIDTH - 500);
+        inner2.add(applyButton); inner2.add().padRight(20);
+        inner2.row();
+        inner2.add().padBottom(10);
+
+        table.add(label).size(labelTexture.getWidth(), labelTexture.getHeight());
+        table.row();
+        table.add().padBottom(space);
+        table.row();
+        table.add(inner1);
+        table.row();
+        table.add().padBottom(space);
+        table.row();
+        table.add(inner2);
 
         stage.addActor(table);
         Gdx.input.setInputProcessor(stage);
@@ -78,12 +152,17 @@ public class MainMenuScreen extends ScreenAdapter {
     }
 
     private void clearScreen() {
-        Gdx.gl.glClearColor(Color.BLACK.r, Color.BLACK.g, Color.BLACK.b, Color.BLACK.a);
+        Gdx.gl.glClearColor((float)135/255, (float)135/255, (float)135/255, 0);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
     }
 
     @Override
     public void dispose() {
         applyButtonTexture.dispose();
+        enableSensorsTexture.dispose();
+        disableSensorsTexture.dispose();
+        labelTexture.dispose();
+        font.dispose();
+        skin.dispose();
     }
 }
